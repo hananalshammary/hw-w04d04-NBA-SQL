bcddf30e5c4560b366fcabb594448c5a1e1e6062
Hanan Darwish <44509220+hananalshammary@users.noreply.github.com>
2018-12-05 22:14:45 +0300

    Update nba_seed.sql

--- nba_seed.sql
+++ nba_seed.sql
@@ -3,15 +3,18 @@
 
 -- Drop DB if exists
 DROP DATABASE IF EXISTS nba_db;
-
+DROP DATABASE nba_db; // I do not have 
 -- Create DB nba_db
 CREATE DATABASE nba_db;
 
+CREATE DATABASE NBA_BD;
+
 -- Connect to nba_db
+\connect nba_bd
 \c nba_db;
 
 -- Create table here
-
+CREATE TABLE players (id SERIAL , name text , age int , team text , games  int , points int);
 
 -- Insert player here
 
@@ -21,7 +24,4 @@ CREATE DATABASE nba_db;
 -- Uncomment COPY players statement and modify it with YOUR file path!
 
 
---COPY players
-  --(name, age, team, games, points)
---FROM '/Users/trevorpreston/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011_2012.csv'
-    --DELIMITER ',' CSV;
+copy players(name, age, team, games, points) from '/Users/hnanalshmry/code/wdi/homework/hw-w04d04-NBA-SQL/nba_season_2011-2012.csv' DELIMITER ',' CSV;
